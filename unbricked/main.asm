229b6db550365ad75f3b26345b2c9933064058a8
Kesara Rathnayake <kesara@fq.nz>
2024-07-13 21:28:00 +1200

    feat: Add Unbricked game

---
+++ unbricked/main.asm
@@ -0,0 +1,60 @@
+INCLUDE "hardware.inc"
+
+Section "Header", ROM0[$100]
+    jp EntryPoint
+    ds $150 - @, 0 ; Make room for the header
+
+EntryPoint:
+    ;Do not turn the LCD off outside of VBlank
+
+WaitVBlank:
+    ld a, [rLY]
+    cp 144
+    jp c, WaitVBlank
+
+    ; Turn the LCD off
+    ld a, 0
+    ld [rLCDC], a
+
+    ; Copy the tile data
+    ld de, Tiles
+    ld hl, $9000
+    ld bc, TilesEnd - Tiles
+
+CopyTiles:
+    ld a, [de]
+    ld [hli], a
+    inc de
+    dec bc
+    ld a, b
+    or a, c
+    jp nz, CopyTiles
+
+    ; Copy the tilemap
+    ld de, Tilemap
+    ld hl, $9800
+    ld bc, TilemapEnd - Tilemap
+
+CopyTilemap:
+    ld a, [de]
+    ld [hli], a
+    inc de
+    dec bc
+    ld a, b
+    or a, c
+    jp nz, CopyTilemap
+
+    ; Turn the LCD on
+    ld a, LCDCF_ON | LCDCF_BGON
+    ld [rLCDC], a
+
+    ; During the first (blank) frame, initialize display registers
+    ld a, %11100100
+    ld [rBGP], a
+
+Done:
+    jp Done
+
+
+INCLUDE "tileset.asm"
+INCLUDE "tilemap.asm"
