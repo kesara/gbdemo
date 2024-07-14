1d6e6c72646abbdf921e772de57a631971b627f6
Kesara Rathnayake <kesara@fq.nz>
2024-07-14 20:19:09 +1200

    feat: Add paddle

--- unbricked/main.asm
+++ unbricked/main.asm
@@ -44,17 +44,91 @@ CopyTilemap:
     or a, c
     jp nz, CopyTilemap
 
+    ; Copy the tile data
+    ld de, Paddle
+    ld hl, $8000
+    ld bc, PaddleEnd - Paddle
+
+CopyPaddle:
+    ld a, [de]
+    ld [hli], a
+    inc de
+    dec bc
+    ld a, b
+    or a, c
+    jp nz, CopyPaddle
+
+    ld a, 0
+    ld b, 160
+    ld hl, _OAMRAM
+
+ClearOam:
+    ld [hli], a
+    dec b
+    jp nz, ClearOam
+
+    ld hl, _OAMRAM
+    ld a, 128 + 16
+    ld [hli], a
+    ld a, 16 + 8
+    ld [hli], a
+    ld a, 0
+    ld [hli], a
+    ld [hl], a
+
+    ; Initialize global variables
+    ld a, 0
+    ld [wFrameCounter], a
+
     ; Turn the LCD on
-    ld a, LCDCF_ON | LCDCF_BGON
+    ld a, LCDCF_ON | LCDCF_BGON | LCDCF_OBJON
     ld [rLCDC], a
 
     ; During the first (blank) frame, initialize display registers
     ld a, %11100100
     ld [rBGP], a
+    ld a, %11100100
+    ld [rOBP0], a
 
-Done:
-    jp Done
+Main:
+    ld a, [rLY]
+    cp 144
+    jp nc, Main
 
+WaitVBlank2:
+    ld a, [rLY]
+    cp 144
+    jp c, WaitVBlank2
+
+    ld a, [wFrameCounter]
+    inc a
+    ld [wFrameCounter], a
+    cp a, 15 ; Every 15 frames (a quarter of a second), run the following code
+    jp nz, Main
+
+    ; Reset the frame counter back to 0
+    ld a, 0
+    ld [wFrameCounter], a
+
+    ; Move the paddle one pixel to the right.
+    ld a, [_OAMRAM + 1]
+    inc a
+    ld [_OAMRAM + 1], a
+    jp Main
 
 INCLUDE "tileset.asm"
 INCLUDE "tilemap.asm"
+
+Paddle:
+    dw `13333331
+    dw `30000003
+    dw `13333331
+    dw `00000000
+    dw `00000000
+    dw `00000000
+    dw `00000000
+    dw `00000000
+PaddleEnd:
+
+SECTION "Counter", WRAM0
+wFrameCounter: db
